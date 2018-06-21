1159dfcd07dbe2a2dc8a132d2dc03f7b6dad2040
xinghaoyefengk <898709886@qq.com>
2018-06-21 21:57:09 +0800

    first commit

---
+++ workspace/my-app/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Records from './components/Records';
+import './index.css';
+
+ReactDOM.render(
+  <Records />,
+  document.getElementById('root')
+);
